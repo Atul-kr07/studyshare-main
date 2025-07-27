3b59d2456e1eafdb44d923392761eb46cac03783
kaushal kumar <kaushalkumar@kaushals-MacBook-Air.local>
2025-07-27 12:00:41 +0530

    Remove deprecated mongoose options

--- backend/index.js
+++ backend/index.js
@@ -49,10 +49,7 @@ app.use(cookieParser());
 app.use(passport.initialize());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
